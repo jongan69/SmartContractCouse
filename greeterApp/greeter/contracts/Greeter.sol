3ba4922d60762143818b939093c87a4432487f2f
Jonathan Gan <jon@cryptocurrensea.io>
2020-12-02 10:24:28 -0500

    Setting up dev env

---
+++ greeterApp/greeter/contracts/Greeter.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Greeter {
+    string greeting;
+
+    function greeter(string _greeting) public {
+        greeting = _greeting;
+    }
+
+    function greet() constant returns (string) {
+        return greeting;
+    }
+}
